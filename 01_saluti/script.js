79d662a06a63236770e2022c1cae7f3c0610e611
simone-sampino <s.samp98@gmail.com>
2025-06-04 14:49:20 +0200

    Refactor greeting function and improve code formatting

--- 01_saluti/script.js
+++ 01_saluti/script.js
@@ -1,13 +1,15 @@
 /* Scrivi una funzione che accetti una stringa contenente un nome e restituisca un saluto seguito dal nome fornito (es. "Ciao [nome]") */
 
-const userName = 'Mario';
-
+const userName = "Mario";
 
 // Dichiara la funzione qui.
 
+function firstName() {
+  console.log("Ciao " + userName);
+}
 
 // Invoca la funzione qui e stampa il risultato in console
 
-
+firstName();
 
 //Risultato atteso se si passa 'Mario': // ciao Mario
