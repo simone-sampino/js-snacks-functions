5e3f7737e3b503a7695e8eab0fefcf2adf9c6bcf
simone-sampino <s.samp98@gmail.com>
2025-06-04 16:03:44 +0200

    Refactor startLetter function for clarity and structure

--- 03_filtra_iniziali/script.js
+++ 03_filtra_iniziali/script.js
@@ -2,12 +2,22 @@
 
 const names = ["Anna", "Luca", "Marco", "Adele", "Laura", "Alessandra"];
 
-
 // Dichiara la funzione qui.
 
+function startLetter() {
+  let letter = [];
+  for (let i = 0; i < names.length; i++) {
+    const element = names[i];
+
+    if (element.charAt(0) === "A") {
+      letter.push(element);
+    }
+  }
+  return letter;
+}
 
 // Invoca la funzione qui e stampa il risultato in console
 
-
+console.log(startLetter());
 
 //Risultato atteso se si passa la lettera A: ["Anna", "Adele", "Alessandra"]
