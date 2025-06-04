06ebd520d36b5fbebab75cac3aaadae1795f24a0
simone-sampino <s.samp98@gmail.com>
2025-06-04 15:36:43 +0200

    Implement initials function to extract first letters from names array

--- 02_iniziali/script.js
+++ 02_iniziali/script.js
@@ -2,12 +2,19 @@
 
 const names = ["Anna", "Luca", "Marco", "Adele", "Giovanni", "Alessandra"];
 
-
 // Dichiara la funzione qui.
 
+function initials() {
+  let firstLetter = [];
+  for (let i = 0; i < names.length; i++) {
+    const element = names[i];
+    firstLetter.push(element.charAt(0));
+  }
+  return firstLetter;
+}
 
 // Invoca la funzione qui e stampa il risultato in console
 
-
+console.log(initials());
 
 //Risultato atteso: ["A", "L", "M", "A", "G", "A"]
